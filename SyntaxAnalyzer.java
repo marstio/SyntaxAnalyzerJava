4ec4271605c4c3488a4e2eb65a29ceb9e6d4863a
Julian Ramirez <julian.gabramirez.04@gmai.com>
2024-10-14 11:32:36 +0800

    second more refined try

--- SyntaxAnalyzer.java
+++ SyntaxAnalyzer.java
@@ -17,13 +17,17 @@ public class SyntaxAnalyzer {
 
         while (true) {
             StringBuilder codeBlock = new StringBuilder();
-            System.out.println("Enter your block of Java code (type 'done' on a new line to finish and analyze, or 'quit' to exit the program):");
+            System.out.println("\nWelcome to Syntax Analyzer! Input your code below: ");
+            System.out.println("(Type 'done' on a new line to finish and analyze, or 'quit' to exit the program):");
+
 
             while (true) {
                 String input = scanner.nextLine();
                 if (input.trim().equalsIgnoreCase("done")) {
                     System.out.println("\nAnalyzing the code block...\n");
                     analyzeBlock(codeBlock.toString());
+
+                    System.out.println("\n------------- End of analysis -------------\n");
                     break;
                 }
                 if (input.trim().equalsIgnoreCase("quit")) {
@@ -52,6 +56,8 @@ public class SyntaxAnalyzer {
             StringReader reader = new StringReader(input);
             StreamTokenizer tokenizer = new StreamTokenizer(reader);
             tokenizer.ordinaryChar('.');
+            tokenizer.ordinaryChar('-');
+            tokenizer.ordinaryChar('/');
             tokenizer.wordChars('_', '_');
             tokenizer.wordChars('$', '$');
             tokenizer.quoteChar('"');
@@ -200,36 +206,181 @@ public class SyntaxAnalyzer {
             return;
         }
 
-        StringBuilder output = new StringBuilder();
-        boolean firstArg = true;
+        StringBuilder expression = new StringBuilder();
+        boolean insideString = false;
+        int parenCount = 1;
 
-        while ((token = tokenizer.nextToken()) != ')') {
-            if (!firstArg && token == '+') {
-                token = tokenizer.nextToken();
+        while ((token = tokenizer.nextToken()) != StreamTokenizer.TT_EOF) {
+            switch (token) {
+                case '"':
+                    insideString = !insideString;
+                    expression.append('"').append(tokenizer.sval).append('"');
+                    break;
+                case '\'':
+                    expression.append('\'').append(tokenizer.sval).append('\'');
+                    break;
+                case StreamTokenizer.TT_NUMBER:
+                    expression.append(tokenizer.nval);
+                    break;
+                case StreamTokenizer.TT_WORD:
+                    if (tokenizer.sval.equals("true") || tokenizer.sval.equals("false")) {
+                        expression.append(tokenizer.sval);
+                    } else if (variables.containsKey(tokenizer.sval)) {
+                        expression.append(variables.get(tokenizer.sval));
+                    } else {
+                        expression.append(tokenizer.sval);
+                    }
+                    break;
+                case '(':
+                    parenCount++;
+                    expression.append('(');
+                    break;
+                case ')':
+                    parenCount--;
+                    if (parenCount == 0) {
+                        String result = evaluateExpression(expression.toString());
+                        System.out.println("Line " + lineNumber + ": Syntax is Valid, System." + outputStream + "." + printType + " statement, Output: " + result);
+                        return;
+                    }
+                    expression.append(')');
+                    break;
+                default:
+                    expression.append((char) token);
+                    break;
             }
-
-            if (token == '"' || token == '\'') {
-                output.append(tokenizer.sval);
-            } else if (token == StreamTokenizer.TT_NUMBER) {
-                output.append(tokenizer.nval);
-            } else if (token == StreamTokenizer.TT_WORD) {
-                if (tokenizer.sval.equals("true") || tokenizer.sval.equals("false")) {
-                    output.append(tokenizer.sval);
-                } else if (variables.containsKey(tokenizer.sval)) {
-                    output.append(variables.get(tokenizer.sval));
-                } else {
-                    System.out.println("Line " + lineNumber + ": Invalid print statement: Undefined variable " + tokenizer.sval);
-                    return;
-                }
-            } else {
-                System.out.println("Line " + lineNumber + ": Invalid print statement: Unexpected token");
-                return;
-            }
-
-            firstArg = false;
         }
 
-        System.out.println("Line " + lineNumber + ": Syntax is Valid, System." + outputStream + "." + printType + " statement, Output: " + output.toString());
+        System.out.println("Line " + lineNumber + ": Invalid print statement: Unmatched parentheses");
+    }
+
+    private static String evaluateExpression(String expression) {
+        try {
+            // Try to evaluate the expression as a mathematical operation first
+            double result = evaluateMathExpression(expression);
+            // Check if the result is a whole number or not
+            if (result == (int) result) {
+                return String.valueOf((int) result);
+            } else {
+                return String.valueOf(result);
+            }
+        } catch (Exception e) {
+            // If not a math expression, fall back to string concatenation handling
+            if (expression.contains("+")) {
+                String[] parts = expression.split("\\+");
+                StringBuilder result = new StringBuilder();
+                for (String part : parts) {
+                    part = part.trim();
+                    if (part.startsWith("\"") && part.endsWith("\"")) {
+                        result.append(part.substring(1, part.length() - 1));
+                    } else if (variables.containsKey(part)) {
+                        result.append(variables.get(part));
+                    } else {
+                        result.append(part);
+                    }
+                }
+                return result.toString();
+            }
+        }
+        return expression;
+    }
+
+
+    //lowkey I think we can remove this method na but I dont want to sira
+    private static String evaluateSimpleExpression(String expression) {
+        // Handle boolean values
+        if (expression.equals("true") || expression.equals("false")) {
+            return expression;
+        }
+
+        // Handle character literals
+        if (expression.startsWith("'") && expression.endsWith("'") && expression.length() == 3) {
+            return expression.substring(1, 2);
+        }
+
+        // Handle variables
+        if (variables.containsKey(expression)) {
+            return String.valueOf(variables.get(expression));
+        }
+
+        // If it's not a boolean, character, or variable, try to evaluate as a math expression
+        try {
+            double result = evaluateMathExpression(expression);
+            if (result == (int) result) {
+                return String.valueOf((int) result);  // Return as integer if it's a whole number
+            } else {
+                return String.valueOf(result);  // Return as double with full precision
+            }
+        } catch (Exception e) {
+            // If it's not a valid math expression, return as is (likely a string)
+            return expression.replaceAll("\"", ""); // Remove quotes from string literals
+        }
+    }
+
+    private static double evaluateMathExpression(String expression) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < expression.length()) ? expression.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < expression.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                return x;
+            }
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if (eat('+')) x += parseTerm();
+                    else if (eat('-')) x -= parseTerm();
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if (eat('*')) x *= parseFactor();
+                    else if (eat('/')) {
+                        double y = parseFactor();
+                        if (y == 0) throw new ArithmeticException("Division by zero");
+                        x /= y;
+                    }
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return +parseFactor();
+                if (eat('-')) return -parseFactor();
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) {
+                    x = parseExpression();
+                    if (!eat(')')) throw new RuntimeException("Missing ')'");
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') {
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(expression.substring(startPos, this.pos));
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char)ch);
+                }
+
+                return x;
+            }
+        }.parse();
     }
 
     private static void handleScannerDeclaration(StreamTokenizer tokenizer, int lineNumber) throws IOException {
@@ -239,7 +390,6 @@ public class SyntaxAnalyzer {
             System.out.println("Line " + lineNumber + ": Invalid Scanner variable name: " + varName);
             return;
         }
-
         tokenizer.nextToken(); // =
         tokenizer.nextToken(); // new
         tokenizer.nextToken(); // Scanner
